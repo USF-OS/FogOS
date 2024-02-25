d9919b23417b9977034b56d15dee17fec56b70ef
Marcus Altman <maaltman@gojira.cs.usfca.edu>
2024-02-25 11:29:43 -0800

    Complete with Full-Byte Lookup Table

--- user/cksum.c
+++ user/cksum.c
@@ -3,19 +3,18 @@
 #include "kernel/stat.h"
 #include "kernel/fcntl.h"
 
-#define POLY32LE 0xEDB88320
-
 int
 main(int argc, char* argv[])
 {
 	uint32 checksum;
 	struct stat metadata;
 
+	// Gets the file name from argv, opens the file, and retrieves the metadata
 	char* file_name = argv[1];
 	int fd = open(file_name, O_RDONLY);
 	fstat(fd, &metadata);
 
-	// Check that the name provided is a valid file name
+	// Checks that the name provided is a valid file name
 	if (metadata.type == 1)
 	{
 		printf("cksum: %s: Is a directory\n", file_name);
@@ -27,10 +26,10 @@ main(int argc, char* argv[])
 		return -1;
 	}
 
-	// Perform the 32-bit CRC
+	// Calculates the 32-bit CRC checksum
 	checksum = crc(fd, metadata.size);
 	
-
+	// Prints the checksum, the file size, and the file name to the console
 	printf("%d %d %s\n", checksum, metadata.size, file_name);
 
 	return 0;
