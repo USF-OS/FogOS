d26bf5a04a1853fc458e9d575f7a84d1f2288713
Noga Gottlieb <ngottlieb@gojira.cs.usfca.edu>
2024-02-26 12:18:24 -0800

    original user.h

--- user/user.h
+++ user/user.h
@@ -40,12 +40,3 @@ int atoi(const char*);
 int memcmp(const void *, const void *, uint);
 void *memcpy(void *, const void *, uint);
 
-//math.c
-#define NaN (0.0 / 0.0)
-
-double pow(double x, double y);
-double sqrt(double x);
-double exp(double x);
-double floor(double x);
-double ceil(double x);
-int closestInt(double x);
