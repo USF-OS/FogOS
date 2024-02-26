4b8987e254b0aaa460e3413b58bfb55b6239b847
Ainslee Smith <apsmith3@dons.usfca.edu>
2024-02-26 15:29:57 -0800

    queue.c deadcode removed

--- user/queue.c
+++ user/queue.c
@@ -18,14 +18,9 @@ int back = -1;
 //check if queue is full 
 int 
 isFull(){
-	/*if (front = 0 && back = 0 || front = 0 && back = 99){
-		return 1;
-	}*/
-	//when back+1 == front
 	if(((back + 1) == front) || ((front == 0) && (back == 10 -1))){
 		return 1; //yes it is full;
 	}
-	
 	return 0; //no it is not full
 }
 
@@ -41,28 +36,12 @@ isEmpty() {
 //remove an element
 void 
 deQueue() {
-	/*if(isFull() == 1){
-		front++;
-		//back == 0;
-		return 1; //sucessfully dequeued
-	}*/
-	//check if the queue is empty 
-	/*if(isEmpty() == 0){
-		queue[back] == NULL;
-		front++;	
-	}*/
-	
-	
-	//return the value pointed by front
-	// inscrese the front index by 1
-	// last element --> resent the values of front and back to -1
 	if (front == back){
 		front = -1;
 		back = -1;
 	}
 	else{
 		front = (front +1) % 10;	
-		//back = (back%10);
 	}
 }
 
@@ -70,75 +49,35 @@ deQueue() {
 //add an element
 void 
 enQueue(char *element) {
-	printf("element to be queued: %s\n", element);
-	/* TODO: we shouldn't check if its full, instead refactor this to remove an 
-	element from the end and add the new elenent to the history queue to keep relevance*/
-
-	
-
 	if (isFull()){
-    	printf("Cannot add element. Queue is full\n");
     	deQueue();
-    	back = (back+1) % 10; //is this right??? not circular
+    	back = (back+1) % 10; 
     	char *a = (char *)malloc(strlen(element)+1);
     	strcpy(a, element);
-    	    	
         queue[back] = a;
 	}
-	/*if (back == 0){
-		//deQueue the last element
-		// add new element --> remove else 
-	}*/
   	else { //add element to queue
-  		//printf("in enqueue\n");
   		//if the queue is empty then update front to 0
     	if (front == -1){
     		front = 0;
     	}
     	//increase the back by 1 but if it reaches end (size) should be start of queue
-    	back = (back+1) % 10; //is this right??? not circular
-    	/*if(back == 10){
-    	   back = 0;
-    	}*/
-    	
+    	back = (back+1) % 10; 
     	char *a = (char *)malloc(strlen(element)+1);
     	strcpy(a, element);
-    	
 		queue[back] = a;
-		
-      	//printf("the queue after enQueue: %s\n", queue);
   	}
 }
 
 //print the queue
 void printQueue(){
-	printf("in printQueue\n");
-	printf("front: %d\n", front);
-	printf("back: %d\n", back);
-	//for(int i = front; i != back; i++){ //is this circular tho???
-	//for(int i = front; i < back+1; i++){
-	//char *temp[10];
 	int count = 0;
 	for (int i = front; i+1 != back; i++) {
-	//for (int i = front; i != back; i = i%10) {
 		  if (count == 10){
 		  	break;
 		  }
 		  i = (i) % 10;
-	      //if (i != '\0'){
-	      printf("in the printqueue for loop\n");
 	      printf("%s ", queue[i]);
 	      count++;
-	      //}
 	}
-	
-	/*for(int i = front; i != back; i++){
-		if (i != '\0'){
-			printf("in the printqueue for loop\n");
-			printf("%s ", queue[i]);
-		}
-		if(i == 9){
-			i = 0;
-		}
-	}*/
 }
