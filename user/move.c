104b3e8e544c144afcf84cd1011c1e3003deb666
Alexander Bareli <abareli@gojira.cs.usfca.edu>
2024-02-26 03:25:08 -0800

    Project 1 (Bareli) | Added: copy and move | TODO: pwd

--- user/move.c
+++ user/move.c
@@ -3,23 +3,29 @@
 #include "kernel/fcntl.h"
 #include "user/user.h"
 
-char buf[2048];
-
 int
 main(int argc, char *argv[])
 {
 	int source;
 	
 	if (argc <= 1){
-		printf("Usage: rm source destination...\n");
+		fprintf(2, "Usage: /move file_source dir_destination...\n");
 		exit(1);
 	} 
 
 	if ((source = open(argv[1], O_RDONLY)) == 0) {
-		printf("Unable to open source file");
+		fprintf(2, "move: Unable to open source file %s\n", argv[1]);
 		exit(1);
 	}
 
-	dup(source);
-	exit(1);
+	char *destination = malloc(strlen(argv[1]) + strlen(argv[2]) + 2);
+	strcpy(destination, argv[2]);
+	destination[strlen(argv[2])] = '/';
+	strcpy( destination + strlen(argv[2]) + 1, argv[1]);
+	if (link(argv[1], destination) < 0) {
+		fprintf(2, "move: link failed %s\n", destination);
+		exit(1);
+	}
+	unlink(argv[1]);
+	exit(0);
 }
