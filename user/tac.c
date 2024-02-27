eae50c6ae1687f38ca1a89162804172573904ba6
Katy Huang <dhuang24@gojira.cs.usfca.edu>
2024-02-26 18:09:31 -0800

    clean up code

--- user/tac.c
+++ user/tac.c
@@ -1,12 +1,13 @@
-#include "kernel/types.h"   // for using uint
-#include "user/user.h"      // for using printf
-#include "kernel/fcntl.h"   // for using malloc and free
+#include "kernel/types.h"   
+#include "user/user.h"      
+#include "kernel/fcntl.h"   
 
 
 // separator input from command line can't contain more than 5 spaces
+// separator can't contain ()
+// separator must have no more than 50 characters
 
 uint init_num_lines = 128;
-
 int has_separator = 0;
 int match_start = -1;
 int math_line_index = -1;
@@ -14,7 +15,7 @@ int match_found = 0;
 char needle[100] = {0};
 
 
-int strStr(char* haystack, char* needle) {
+int search_str(char* haystack, char* needle) {
   int needle_len = strlen(needle);
   int last_start = strlen(haystack) - needle_len;
 
@@ -22,11 +23,11 @@ int strStr(char* haystack, char* needle) {
   {
       for (int i = 0; i < needle_len; i++) 
       {
-          if (*(needle+i) != *(haystack+start+i)) 
+          if (*(needle + i) != *(haystack + start + i)) 
           {
             break;
           }
-          if (i == (needle_len-1)) 
+          if (i == (needle_len - 1)) 
           {
             return start;
           }
@@ -51,29 +52,25 @@ print_lines(char **lines, int num_lines)
   {
      if (match_found == 0) 
      {
-        printf("tac: the string is not contained in the input text\n");
+        fprintf(2, "tac: the string is not contained in the input text\n");
         exit(1);
      }
-    // 1. print lines[math_line_index] starting from index "match_start" to the end of this line
+
     print_substring(lines[math_line_index], match_start, strlen(lines[math_line_index])-1);
 
-    // 2. print all lines beginning from (math_line_index+1) to the end of file (the last line_index)
     for (int i = math_line_index+1; i < num_lines; i++) 
     {
       printf("%s", lines[i]);
       free(lines[i]);
     }
 
-    // 3. print all lines from lines[0] to lines[math_line_index-1]
     for (int i = 0; i <= math_line_index-1; i++) 
     {
       printf("%s", lines[i]);
       free(lines[i]);
     }
 
-    // 4. print lines[math_line_index] starting from index 0 to lines[match_start-1]
     print_substring(lines[math_line_index], 0, match_start-1);
-
     free(lines[math_line_index]);
   }
   else // no separator
@@ -89,9 +86,6 @@ print_lines(char **lines, int num_lines)
       }
     }
   }
-
-
-
 }
 
 void
@@ -104,7 +98,8 @@ tac(int fd)
 
   if (lines == 0) 
   {
-    printf("tac: malloc for lines failed\n");
+    fprintf(2, "tac: malloc failed\n");
+    exit(1);
   }
 
   char *line = 0;
@@ -119,16 +114,21 @@ tac(int fd)
   
     if (char_read <= 0)
     {
-      // reach EOF
+      // reach end of file
       break;
     } 
     else if (line_index == (new_num_lines - 1))
-    { // the last line of the file read has some text instead of EOF
-      // expand the capacity of memory storing lines
+    { 
       old_num_lines = new_num_lines;
       new_num_lines = old_num_lines * 2;
 
       new_lines = malloc(new_num_lines * sizeof(char*));
+
+      if (new_lines == 0) 
+      {
+        fprintf(2, "tac: malloc failed\n");
+        exit(1);
+      }
 
       memcpy(new_lines, lines, (line_index + 1) * sizeof(char*));
 
@@ -144,12 +144,12 @@ tac(int fd)
     } 
     else 
     {
-      lines[line_index] = line; // won't store the line in the lines array if it's EOF
+      lines[line_index] = line; 
     }
     
     if (has_separator == 1 && match_found == 0)
     { 
-      int result = strStr(line, needle);
+      int result = search_str(line, needle);
 
       if (result != -1)
       {
@@ -164,10 +164,6 @@ tac(int fd)
 
     line_index++;
   }
-
-  // print the reversed text
-  // if no -s, print and free lines in reversed order
-  // if has -s, 
 
   int line_count = line_index;
   print_lines(lines, line_count);
@@ -187,42 +183,24 @@ main(int argc, char *argv[])
     exit(1);
   }
 
-  for (int i = 1; i < argc; i++)
-  {
-    printf("argv: %s\n", argv[i]);
-  }
-
   // if argc is not 2, make sure argc is 4 (make sure quoted string is treated as one argument)
   // after quote string is treated as one argument, if argc is still not 4, print error message and exit
   if (argc != 2) 
   {
     
     int c = 0;
-    // 1. go through each current argument, find the first '"' (should be the first char of an argument)
+
     for (int i = 1; i < argc; i++)
     {
       
-      if (argv[i][0] == '"')   // "a
+      if (argv[i][0] == '"')   
       {
-        char *arg = argv[i];  // for holding the string arg
+        char *arg = argv[i];  
 
-        
         char new_str_arg[100] = {0};
     
-        const char *space = " \0";
-        // printf("---------------------------\n");
-        // printf("arg is: %s\n", arg);
-        // printf("new_str_arg is: %s\n", new_str_arg);
-
         strcpy(new_str_arg, arg);
         strcpy(new_str_arg + strlen(arg), "\0");
-        // printf("new_str_arg is: %s\n", new_str_arg);
-        //strcpy(new_str_arg + strlen(arg), space);
-
-        // printf("strlen(arg) is: %d\n", strlen(new_str_arg));
-        // printf("strlen(new_str_arg) is: %d\n", strlen(new_str_arg));
-        // printf("---------------------------\n");
-
 
         int arg_start_index = i;
 
@@ -230,46 +208,17 @@ main(int argc, char *argv[])
           fprintf(2, "tac: invalid arguments\n");
           exit(1);
         }
-        printf("i: %d, str arg is: %s\n", i, arg);
-        // if the current argument start with ", search for " at the end of this argument
-        // otherwise, move on to the next argument in argv
-        // if the close " is at the end of the same argument, no need to change argc and argv
-        // if the close " is found at the end of other argument, change argc and argv
-        // if the close " is not found in the end of other argument, print error message and exit (invalid string argument)
+
         while (new_str_arg[strlen(new_str_arg) - 1] != '"')
         {
-          printf("new_str_arg[strlen(new_str_arg) - 1] is: %c\n", new_str_arg[strlen(new_str_arg) - 1]);
           i++;
 
           if (i < argc) 
-          { // char* strcpy(char*, const char*);
-            printf("----------A-------------------\n");
-            printf("argv[0]: %s\n", argv[0]);
-            printf("argv[1]: %s\n", argv[1]);
-            printf("argv[2]: %s\n", argv[2]);
-            printf("argv[3]: %s\n", argv[3]);
-            
-
-            //const char *space = " \0";
-            printf("strlen(new_str_arg): %d\n", strlen(new_str_arg));
-            printf("Before strcpy, i: %d, new_str_arg is: %s\n", i, new_str_arg);
-
-            
-            strcpy(new_str_arg + strlen(new_str_arg), space);
+          { 
+            strcpy(new_str_arg + strlen(new_str_arg), " \0");
             strcpy(new_str_arg + strlen(new_str_arg), argv[i]);
+            strcpy(new_str_arg + strlen(new_str_arg) + strlen(argv[i]), "\0");   
 
-            strcpy(new_str_arg + strlen(new_str_arg) + strlen(argv[i]), "\0");
-
-
-            
-            printf("After strcpy, i: %d, new_str_arg is: %s\n", i, new_str_arg);
-
-            //printf("----------B-------------------\n");
-            // printf("argv[0]: %s\n", argv[0]);
-            // printf("argv[1]: %s\n", argv[1]);
-            // printf("argv[2]: %s\n", argv[2]);
-            // printf("argv[3]: %s\n", argv[3]);
-            
             c++;
           }
           else
@@ -277,11 +226,8 @@ main(int argc, char *argv[])
             break;
           }
         }
-        printf("arg_start_index: %d\n", arg_start_index);
-
 
         argv[arg_start_index] = new_str_arg;
-        printf("argv[arg_start_index]: %s\n", argv[arg_start_index]);
 
         if (arg_start_index == 2 )
         {
@@ -290,41 +236,24 @@ main(int argc, char *argv[])
       }
     }
 
-    
     argc -= c;
-
-    for (int i = 1; i < argc; i++)
-    {
-      printf("argv: %s\n", argv[i]);
-    }
-
-    printf("argc: %d\n", argc);
   }
 
-  // remove this block later (if string argument includes spaces, it will mess up argc)
   if (argc != 2 && argc != 4)
   {
     fprintf(2, "tac: invalid arguments\n");
-    exit(1); // not handling reading from stdin for now
+    exit(1);
   }
-
-  //return 0; // remove later
 
   int fd;
   const char* file_path;
 
-  if (argc == 2)    // if argc is 2, the second argument must be a fileName. Example: tac fileName.c  
+  if (argc == 2)  
   {
     file_path = argv[1];
   }
-  else // argc == 4, example: tac fileName.c -s "main"   ||  tac -s "main" fileName.c
-  {                      //   tac fileName.c -s "A B C"  ||  tac -s "A B C" fileName.c      
-    // check if -s and a string are contained in the arguments
-    //  go through arguemnts one by one, if -s is found, check if the next argument is a "string"
-    //  invalid cases:  no -s
-    //                  -s is not followed by a "string"
-    // if true, search for the fileName argument (if pos of -s is 1/2, pos of file is 3/1; )
-    // otherwise, invalid arguments
+  else 
+  {                     
     int i;
     for (i = 1; i < argc; i++) 
     {
@@ -334,8 +263,6 @@ main(int argc, char *argv[])
       {
         // find -s
         // check if argv[i+1] is a string
-        printf("i+1: %d, argc is: %d\n", i+1, argc);
-
         if (i+1 < argc && strlen(argv[i+1]) > 2 
               && argv[i+1][0] == '"' && argv[i+1][strlen(argv[i+1]) - 1] == '"') 
         {
@@ -363,7 +290,7 @@ main(int argc, char *argv[])
   }
 
 
-  fd = open(file_path, 0); //from user.h: int open(const char*, int);. file path = argv[i], omode = 0
+  fd = open(file_path, 0);
   if (fd < 0) 
   {
     fprintf(2, "tac: cannot open %s\n", file_path);
